0569d46baa24e0ab548b40be1251b7b74bcdbd06
Yong Huang <yong.huang.sh@gmail.com>
2019-01-08 22:07:06 +0800

    Add ingress yaml and commands to recreate the ingress everytime

--- priv/deploy-dev.sh
+++ priv/deploy-dev.sh
@@ -3,3 +3,7 @@
 kubectl delete deploy/web0-dev
 kubectl delete svc/web0-dev
 kubectl create -f priv/deploy-dev.yaml
+
+# No kubectl path for ingress, just delete and create again
+kubectl delete ing/davidhuang-top-ingress 
+kubectl create -f priv/ingress.yaml
