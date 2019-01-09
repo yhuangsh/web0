ae2d6982b2182896c75933e15a8f46ede1f345e2
Yong Huang <yong.huang.sh@gmail.com>
2019-01-10 01:10:37 +0800

    Fix a hostname error and add , as separator

--- src/web0_hdlr_index.erl
+++ src/web0_hdlr_index.erl
@@ -9,12 +9,15 @@ init(Req0, State) ->
 content_type() -> #{<<"content-type">> => <<"text/text">>}.
 
 hello_msg() ->
-    P1 = net_adm:ping('web0@web0-1.web0.default.svc.cluster.local'),
-    P2 = net_adm:ping('web0@web0-2.web0.default.svc.cluster.local'),
-    P3 = net_adm:ping('web0@web0-3.web0.default.svc.cluster.local'),
+    P1 = net_adm:ping('web0@web0-0.web0.default.svc.cluster.local'),
+    P2 = net_adm:ping('web0@web0-1.web0.default.svc.cluster.local'),
+    P3 = net_adm:ping('web0@web0-2.web0.default.svc.cluster.local'),
     {ok, Hostname} = inet:gethostname(),
     ["Hello from web0 app in Erlang with Cowboy, served from: \n",
      "hostname=", Hostname, "\n",
      "node=", atom_to_list(node()), "\n",
-     "pinging nodes=[", lists:map(fun(N) -> atom_to_list(N) end, [P1, P2, P3]), "]\n",
-     "connected nodes=[", lists:map(fun(N) -> atom_to_list(N) end, nodes()), "]\n"].
+     "pinging nodes=[", mk_list([P1, P2, P3]), "]\n",
+     "connected nodes=[", mk_list(nodes()), "]\n"].
+
+mk_list(L) ->
+    lists:join(",", lists:map(fun(N) -> atom_to_list(N) end, L)).
