395fe40c232537dad182ef570cb6c0fdb62feabb
Yong Huang <yong.huang.sh@gmail.com>
2019-01-10 01:01:02 +0800

    Add pinging of other nodes

--- src/web0_hdlr_index.erl
+++ src/web0_hdlr_index.erl
@@ -9,8 +9,12 @@ init(Req0, State) ->
 content_type() -> #{<<"content-type">> => <<"text/text">>}.
 
 hello_msg() ->
+    P1 = net_adm:ping('web0@web0-1.web0.default.svc.cluster.local'),
+    P2 = net_adm:ping('web0@web0-2.web0.default.svc.cluster.local'),
+    P3 = net_adm:ping('web0@web0-3.web0.default.svc.cluster.local'),
     {ok, Hostname} = inet:gethostname(),
     ["Hello from web0 app in Erlang with Cowboy, served from: \n",
      "hostname=", Hostname, "\n",
      "node=", atom_to_list(node()), "\n",
-     "cookie=", atom_to_list(erlang:get_cookie()), "\n"].
+     "pinging nodes=[", lists:map(fun(N) -> atom_to_list(N) end, [P1, P2, P3]), "]\n",
+     "connected nodes=[", lists:map(fun(N) -> atom_to_list(N) end, nodes()), "]\n"].
