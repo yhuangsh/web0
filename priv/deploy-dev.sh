eb593a1b12fc5238148a03a52b7618d83a782172
Yong Huang <yong.huang.sh@gmail.com>
2019-01-11 23:13:12 +0800

    Simplify redeploy script and comment out ingress delete/create

--- priv/deploy-dev.sh
+++ priv/deploy-dev.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
-kubectl delete sts/web0
-kubectl delete svc/web0
-kubectl create -f priv/deploy-dev.yaml
+kubectl replace -f priv/deploy-dev.yaml
 
 # No kubectl path for ingress, just delete and create again
-kubectl delete ing/davidhuang-top-ingress 
-kubectl create -f priv/ingress.yaml
+#kubectl delete ing/davidhuang-top-ingress 
+#kubectl create -f priv/ingress.yaml
