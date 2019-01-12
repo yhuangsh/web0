e128ba615eb5b188b6d28c304f0c023c0f595930
Yong Huang <yong.huang.sh@gmail.com>
2019-01-12 12:06:22 +0800

    Refactor 404 and 200 responses

---
+++ src/web0_hdlr_common.erl
@@ -0,0 +1,13 @@
+-module(web0_hdlr_common).
+
+-export(['404'/2, '200'/2]).
+
+-define(CT_TEXT, <<"content-type">> => <<"text/text">>).
+
+%%====================================================================
+%% API
+%%====================================================================
+
+'404'(R, S) -> {ok, cowboy_req:reply(404, #{?CT_TEXT}, <<"web0: ooops, not found!">>, R), S}.
+'200'(R, S) -> {ok, cowboy_req:reply(200, R), S}.
+
