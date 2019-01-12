d6e5a3dc97b876531ff24c82e301add127d94988
Yong Huang <yong.huang.sh@gmail.com>
2019-01-12 14:29:35 +0800

    Split svc and sts into two yamls and remove deploy script

--- priv/deploy-dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-kubectl replace -f priv/deploy-dev.yaml
-
-# No kubectl path for ingress, just delete and create again
-#kubectl delete ing/davidhuang-top-ingress 
-#kubectl create -f priv/ingress.yaml
