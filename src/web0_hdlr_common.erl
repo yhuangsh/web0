a951705fd4895465c0e8bc0af062284ec372f290
Yong Huang <yong.huang.sh@gmail.com>
2019-01-12 14:37:53 +0800

    Add some printouts to the probes

--- src/web0_hdlr_common.erl
+++ src/web0_hdlr_common.erl
@@ -1,6 +1,6 @@
 -module(web0_hdlr_common).
 
--export(['404'/2, '200'/2]).
+-export(['404'/2, '200'/2, '200'/3]).
 
 -define(CT_TEXT, <<"content-type">> => <<"text/text">>).
 
@@ -10,4 +10,5 @@
 
 '404'(R, S) -> {ok, cowboy_req:reply(404, #{?CT_TEXT}, <<"web0: ooops, not found!">>, R), S}.
 '200'(R, S) -> {ok, cowboy_req:reply(200, R), S}.
+'200'(R, T, S) -> {ok, cowboy_req:reply(200, #{?CT_TEXT}, T, R), S}.
 
