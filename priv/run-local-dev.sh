dac44d6ffa3656506a1cf3e53ee7e0eaf81d536e
Yong Huang <yong.huang.sh@gmail.com>
2019-01-12 12:05:53 +0800

    Mimic single node deployment in dev environment

--- priv/run-local-dev.sh
+++ priv/run-local-dev.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-
 docker run \
     --rm \
-    --name app \
-    --hostname "web0-2.web0.default.svc.cluster.local" \
+    --net web0-net \
+    --name web0-0 \
+    --ip 172.28.0.2 \
+    --hostname web0-0.web0.default.svc.cluster.local \
     -v `pwd`:/project \
     -p 7000:7000 \
     -it \
