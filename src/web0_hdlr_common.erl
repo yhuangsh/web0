6f4d4f051768216c81d6d7b521f0d8b2f01036f8
Yong Huang <yong.huang.sh@gmail.com>
2019-01-19 21:16:49 +0800

    Fix bugs in the 400 and 404 functions

--- src/web0_hdlr_common.erl
+++ src/web0_hdlr_common.erl
@@ -1,6 +1,6 @@
 -module(web0_hdlr_common).
 
--export(['400'/3, '404'/2, '200'/2, '200'/3]).
+-export(['404'/3, '404'/2, '200'/2, '200'/3]).
 
 -define(CT_TEXT, <<"content-type">> => <<"text/text">>).
 
@@ -10,8 +10,8 @@
 
 '200'(R, S) -> reply(200, R, S).
 '200'(T, R, S) -> reply(200, T, R, S).
-'400'(T, R, S) -> reply(400, T, R, S).
-'404'(R, S) -> reply(404, R, <<"web0: ooops, not found!">>, S).
+'404'(T, R, S) -> reply(404, T, R, S).
+'404'(R, S) -> reply(404, <<"web0: ooops, not found!">>, R, S).
 
 reply(C, R, S) -> {ok, cowboy_req:reply(C, R), S}.
 reply(C, T, R, S) -> {ok, cowboy_req:reply(C, #{?CT_TEXT}, T, R), S}.
